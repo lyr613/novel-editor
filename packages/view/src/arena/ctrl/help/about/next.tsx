2faa60edc6252cb761950ebde819c0c0a3111ec5
qsoft <qiusoft613@gmail.com>
2020-07-31 14:47:34 +0800

    save: 记录待做功能

--- packages/view/src/arena/ctrl/help/about/next.tsx
+++ packages/view/src/arena/ctrl/help/about/next.tsx
@@ -9,7 +9,7 @@ export default function Next() {
         <div className={s.Next}>
             <SectionHeader>即将到来</SectionHeader>
             <div className={s.aline}>角色储存图片</div>
-            <div className={s.aline}>单独的角色关系设置页面</div>
+            <div className={s.aline}>地图元素右侧列表可编辑</div>
         </div>
     )
 }
