673f4b3a289ef661b9784a374dbd3a5d02c52a76
qsoft <qiusoft613@gmail.com>
2020-08-31 09:12:28 +0800

    save: 一条注释

--- packages/elec/src/util/main-window.ts
+++ packages/elec/src/util/main-window.ts
@@ -11,5 +11,9 @@ export function create_main_window() {
 
 /** 注意使用一定在创建后使用 */
 export function get_main_window() {
+    // 实际上不应该用此单例模式, 应该保证提前创建
+    // if (!main_window) {
+    //     create_main_window()
+    // }
     return main_window!
 }
