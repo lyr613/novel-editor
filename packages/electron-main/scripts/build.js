e9cb04873ab306bb0b6d92763202962fcdb37e5a
qsoft <qiusoft613@gmail.com>
2020-11-15 17:18:41 +0800

    feat: 尝试在mac上开发elec

--- packages/electron-main/scripts/build.js
+++ packages/electron-main/scripts/build.js
@@ -9,19 +9,19 @@ const opt_src = path.join(__dirname, '..', 'cli', 'webpack-pro.js')
 
 if (plat === 'win32') {
     console.log('在windows上打包')
-    const cdi = cp.exec(`  ${webpack_src} --config ${opt_src}  `, (err, out, de) => {
-        // 这里三个打印都没用
-        console.log(err)
-        console.log(out)
-        console.log(de)
-    })
-    cdi.stdout.on('data', (msg) => {
-        console.log(msg)
-    })
-    cdi.on('close', () => {
-        after_webpack_build()
-    })
 }
+const cdi = cp.exec(`  ${webpack_src} --config ${opt_src}  `, (err, out, de) => {
+    // 这里三个打印都没用
+    console.log(err)
+    console.log(out)
+    console.log(de)
+})
+cdi.stdout.on('data', (msg) => {
+    console.log(msg)
+})
+cdi.on('close', () => {
+    after_webpack_build()
+})
 
 function after_webpack_build() {
     console.log('aaaaa')
--- packages/electron-main/scripts/watch.js
+++ packages/electron-main/scripts/watch.js
@@ -9,13 +9,13 @@ const opt_src = path.join(__dirname, '..', 'cli', 'webpack-dev.js')
 
 if (plat === 'win32') {
     console.log('在windows上启动开发')
-    const cdi = cp.exec(`  ${webpack_src} --config ${opt_src} --watch  `, (err, out, de) => {
-        // 这里三个打印都没用
-        console.log(err)
-        console.log(out)
-        console.log(de)
-    })
-    cdi.stdout.on('data', (msg) => {
-        console.log(msg)
-    })
 }
+const cdi = cp.exec(`  ${webpack_src} --config ${opt_src} --watch  `, (err, out, de) => {
+    // 这里三个打印都没用
+    console.log(err)
+    console.log(out)
+    console.log(de)
+})
+cdi.stdout.on('data', (msg) => {
+    console.log(msg)
+})
