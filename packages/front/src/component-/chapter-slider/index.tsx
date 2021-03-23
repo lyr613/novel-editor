371a2f6de33b97e2d927fb7552ea5ad0f346e279
qsoft <qiusoft613@gmail.com>
2021-03-23 13:29:46 +0800

    feat: 章节滑竿添加卷章名显示

--- packages/front/src/component-/chapter-slider/index.tsx
+++ packages/front/src/component-/chapter-slider/index.tsx
@@ -7,6 +7,7 @@ import { DirectionalHint, IconButton, Slider, Stack } from '@fluentui/react'
 import { useObservable } from 'rxjs-hooks'
 import { SubVolume } from 'subject-/volume'
 import { BehaviorSubject } from 'rxjs'
+import { map } from 'rxjs/operators'
 
 /** 章节下标 */
 export const ChapterSliderIndex$ = new BehaviorSubject(0)
@@ -19,6 +20,7 @@ export default function ChapterSlider() {
         <div className={css(style.ChapterSlider)}>
             <Exact />
             <MainSlider />
+            <NameShow />
         </div>
     )
 }
@@ -45,11 +47,34 @@ function MainSlider() {
                         next_index(n)
                     }}
                     min={0}
-                    max={li.length}
+                    max={li.length - 1}
                     className={css(style.MainSliderSlider)}
                     showValue={false}
                 ></Slider>
             )}
+        </div>
+    )
+}
+
+function NameShow() {
+    const txt = useObservable(
+        () =>
+            ChapterSliderIndex$.pipe(
+                map((i) => {
+                    const chaps = SubVolume.chaper_li
+                    const chap = chaps[i]
+                    const vol = SubVolume.li$.value.find((v) => v.children.find((k) => k.id === chap.id))
+                    const t1 = vol?.name || ''
+                    const t2 = chap.name
+                    return [t1, t2]
+                }),
+            ),
+        ['', ''],
+    )
+    return (
+        <div className={css(style.NameShow)}>
+            <b className={css(style.NameShowVol)}>{txt[0]}</b>
+            <span className={css(style.NameShowChap)}>{txt[1]}</span>
         </div>
     )
 }
--- packages/front/src/component-/chapter-slider/style.ts
+++ packages/front/src/component-/chapter-slider/style.ts
@@ -7,11 +7,15 @@ interface style_vo {
     ChapterSlider: object
     MainSlider: object
     MainSliderSlider: object
+    NameShow: object
+    NameShowVol: object
+    NameShowChap: object
 }
 /** 样式 */
 export const style: style_vo = StyleSheet.create<style_vo>({
     ChapterSlider: {
         display: 'flex',
+        position: 'relative',
         fontSize: 0,
         width: '100%',
         height: 40,
@@ -21,10 +25,41 @@ export const style: style_vo = StyleSheet.create<style_vo>({
         boxSizing: 'border-box',
         width: 'calc(100% - 40px)',
         height: 40,
-        padding: '0 20px 0 20px',
+        padding: '0 80px 0 20px',
         alignItems: 'center',
     },
     MainSliderSlider: {
         width: '100%',
+    },
+    NameShow: {
+        position: 'absolute',
+        right: 0,
+        top: 0,
+        textOverflow: 'ellipsis',
+        overflow: 'hidden',
+        boxSizing: 'border-box',
+        width: '80px',
+        height: 40,
+        padding: '0 5px',
+        lineHeight: '40px',
+        fontSize: 14,
+        textAlign: 'right',
+        backgroundColor: StyleTheme.style_vars.themeLight,
+        userSelect: 'none',
+        whiteSpace: 'nowrap',
+        ':hover': {
+            width: 'unset',
+            backgroundColor: StyleTheme.style_vars.themeTertiary,
+            padding: '0 10px',
+        },
+        ':hover b': {
+            fontSize: 14,
+        },
+    },
+    NameShowVol: {
+        fontSize: 0,
+    },
+    NameShowChap: {
+        padding: '0 0 0 5px',
     },
 })
